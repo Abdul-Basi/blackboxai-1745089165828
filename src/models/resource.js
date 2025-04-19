78a6117c41b139e78e005e97102dab10c271929f
Abdul-Basi <156311120+Abdul-Basi@users.noreply.github.com>
2025-04-20 00:00:03 +0500

    Add src/models/resource.js 1745089204200

---
+++ src/models/resource.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database');
+
+const Resource = sequelize.define('Resource', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = Resource;
